62901ad70fabf3cb72c30899c474497ca7309d9f
morkulden@gmail.com <Port@tebien84>
2021-11-08 11:08:13 +0000

    Added test for CardService & CarsResource.

---
+++ src/main/test/java/org/acme/CarServiceTest.java
@@ -0,0 +1,60 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.acme.model.Car;
+import org.acme.services.CarService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@QuarkusTest
+public class CarServiceTest {
+
+    @Inject
+    CarService carService;
+
+    private Car car;
+
+    @Test
+    public void testCreateCar() {
+        car = new Car(null, "Ford", "Focus","Yellow", 2003);
+
+        Assertions.assertNotNull(carService.create(car));
+    }
+
+    @Test
+    public void testRetrieveCar() {
+        car = new Car(null, "Renault", "Clio","Blue", 1998);
+        car.setId(carService.create(car));
+
+        Assertions.assertEquals(car, carService.find(car.getId()));
+    }
+
+    @Test
+    public void testListCars() {
+        List<Car> cars = Arrays.asList(
+                new Car(null, "Mercedes", "A3","White", 2010),
+                new Car(null, "BMW", "300","Black", 2011)
+        );
+
+        cars.forEach(carService::create);
+
+        List<Car> carResponse = carService.list();
+
+        Assertions.assertTrue(cars.size() >= 2);
+        Assertions.assertTrue(carResponse.containsAll(cars));
+    }
+
+    @Test
+    public void testDeleteCar() {
+        car = new Car(null, "Opel", "Vectra","Orange", 2005);
+        car.setId(carService.create(car));
+        carService.remove(car.getId());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> carService.find(car.getId()));
+    }
+}
---
+++ src/main/test/java/org/acme/CarsResourceTest.java
@@ -0,0 +1,91 @@
+package org.acme;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.acme.model.Car;
+import org.acme.services.CarService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class CarsResourceTest {
+
+    @Inject
+    CarService carService;
+
+    @Test
+    public void testCreateCar() {
+        given()
+                .body("{\"make\": \"Ford\", \"model\": \"Focus\", \"colour\": \"White\", \"year\": 1994}")
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .post("/cars")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    public void testCreateInvalidCar() {
+        given()
+                .body("{\"id\": 1,\"make\": \"Ford\", \"model\": \"Focus\", \"colour\": \"White\", \"year\": 1994}")
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .post("/cars")
+                .then()
+                .statusCode(422);
+    }
+
+    @Test
+    public void testRetrieveCar() {
+        Integer id = carService.create(new Car(null, "Honda", "Civic", "Blue", 2005));
+
+        given()
+                .when().get("/cars/" + id)
+                .then()
+                .statusCode(200)
+                .body(
+                        "make", is("Honda"),
+                        "model", is("Civic"),
+                        "colour", is("Blue"),
+                        "year", is(2005)
+                );
+    }
+
+    @Test
+    public void testList() {
+        List<Car> cars = Arrays.asList(
+                new Car(null, "Mercedes", "A3", "White", 2010),
+                new Car(null, "BMW", "300", "Black", 2011)
+        );
+
+        cars.forEach(carService::create);
+
+        given()
+                .when().get("/cars")
+                .then()
+                .statusCode(200)
+                .body("$.size()", Matchers.greaterThanOrEqualTo(2));
+    }
+
+    @Test
+    public void testDeleteCar() {
+        Integer id = carService.create(new Car(null, "Opel", "Vectra", "Green", 2007));
+
+        given()
+                .when().delete("/cars/" + id)
+                .then()
+                .statusCode(200);
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> carService.find(id));
+    }
+}
